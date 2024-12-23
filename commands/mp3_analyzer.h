e8e732fe12cde8ca61e4d878dd7d9643da945525
Johmaru <hatunehimeka@outlook.jp>
2024-12-23 20:01:42 +0900

    MP3 Analyzer: Add functions for sample rate and bitrate retrieval

--- commands/mp3_analyzer.h
+++ commands/mp3_analyzer.h
@@ -1,7 +1,12 @@
 #ifndef MP3_ANALYZER_H
 #define MP3_ANALYZER_H
+#include <stdio.h>
 
 void mp3_analyzer(const char *file);
 int mp3_analyzer_check(const char *file);
+float get_mp3_sample_rate(const char *file);
+float get_mp3_bitrate(const char *file);
+int is_vbr(FILE *fp);
+float calculate_vbr_bitrate(FILE *fp);
 
 #endif
